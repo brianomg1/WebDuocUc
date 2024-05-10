5fbe9fce201d854c339db2630630be27c78d1856
brianomg1 <elbrianomg1998@gmail.com>
2024-05-10 10:55:01 -0400

    git remote add origin https://github.com/brianomg1/WebDuocUc.git

--- Carte.js
+++ Carte.js
@@ -1,51 +1,51 @@
 let pagina = 1;
-    
-    const Anterior = document.getElementById('Anterior')
-    const Siguiente = document.getElementById('Siguiente')
+const Anterior = document.getElementById('Anterior');
+const Siguiente = document.getElementById('Siguiente');
 
-    Siguiente.addEventListener('click',() => {
-        if (pagina < 1000){
-            pagina +=1;
-            bookesCARD();
-        }
-    });
+Siguiente.addEventListener('click', () => {
+	if(pagina < 1000){
+		pagina += 1;
+		cargarPeliculas();
+	}
+});
 
-    Anterior.addEventListener('click',()=>{
-        if(pagina > 1){
-            pagina -=1;
-            bookesCARD();
-        }
-    });
+Anterior.addEventListener('click', () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPeliculas();
+	}
+});
 
-    const bookesCARD = async() => {
-        try{
-            const respuesta = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+const cargarPeliculas = async() => {
+	try {
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
+	
+		console.log(respuesta);
 
-            console.log(respuesta);
-            if(respuesta.status===200){
-                const datos=await respuesta.json();
+		if(respuesta.status === 200){
+			const datos = await respuesta.json();
+			
+			let peliculas = '';
+			datos.results.forEach(pelicula => {
+				peliculas += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+						<h3 class="titulo">${pelicula.title}</h3>
+					</div>
+				`;
+			});
 
-                let peliculas = '';
-                datos.results.forEach(peliculas => {
-                    peliculas+=`
-                    <div class="peliculas">
-                    <img class = "poster" src= "//image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-                    <h3 class = "pelicasbookes">${peliculas.title}</h3>
-                    </div>
-                    `;
-                });
-                document.getElementById('contenedor').innerHTML=peliculas;
+			document.getElementById('contenedor').innerHTML = peliculas;
 
-            }   else if (respuesta.status === 401){
-                console.log(' llave muy mal');
+		} else if(respuesta.status === 401){
+			console.log('llave mal');
+		} else if(respuesta.status === 404){
+			console.log('La pelicula no tengo buscar vacio');
+		} else {
+			console.log('error que no sabia  que paso?');
+		}
 
-            } else if (respuesta.status === 404){
-                console.log('pelicula no hay buscar');
-            } else {
-                console.log('un error no sabia que paso?')
-            }
-
-          } catch(error){
+	} catch(error){
 		console.log(error);
 	}
 
