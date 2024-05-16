d934ceee15cf3b22b0ddf899b1bf698406a4ca02
brianomg1 <elbrianomg1998@gmail.com>
2024-05-16 16:58:31 -0400

    git remote add origin https://github.com/brianomg1/WebDuocUc.git

--- anime.js
+++ anime.js
@@ -5,6 +5,8 @@ const cargarPeliculas = async() => {
         const respuesta = await fetch('https://api.themoviedb.org/3/movie/550?api_key=5dba216a4a48439eb21fa18dd4387501&language=es-MX');
 
         console.log(respuesta);
+        const datos= await respuesta.json();
+        console.log(datos);
     }catch(error){
         console.log(error);
     }
