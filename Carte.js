ef09dba8c08e9fa7f5ea759858d79503a3a2c948
brianomg1 <elbrianomg1998@gmail.com>
2024-05-17 10:24:27 -0400

    git remote add origin https://github.com/brianomg1/WebDuocUc.git

--- Carte.js
+++ Carte.js
@@ -1,3 +1,46 @@
+const apiKey = '0980dcf64fcba5b2f25674a5a9b10579'; // Reemplaza con tu clave API real
+const baseUrl = 'https://api.themoviedb.org/3';
+const imageBaseUrl = 'https://image.tmdb.org/t/p/w500/'; // URL base para las imágenes
+
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const movieResults = document.getElementById('movie-results');
+
+searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value.trim();
+  if (!searchTerm) {
+    return;
+  }
+
+  fetch(`${baseUrl}/search/movie?api_key=${apiKey}&query=${searchTerm}`)
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+      if (movies.length === 0) {
+        movieResults.innerHTML = '<p>No se encontraron películas.</p>';
+        return;
+      }
+
+      movieResults.innerHTML = '';
+      movies.forEach(movie => {
+        const movieTitle = movie.title;
+        const moviePosterPath = movie.poster_path ? `${imageBaseUrl}${movie.poster_path}` : 'placeholder.jpg'; // Ruta completa del póster
+
+
+        const movieElement = document.createElement('div');
+        movieElement.className = 'movie-result';
+        movieElement.innerHTML = `
+          <img src="${moviePosterPath}">
+          <h3>${movieTitle}</h3>
+        `;
+
+        movieResults.appendChild(movieElement);
+      });
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+
 let pagina = 1;
 const Anterior = document.getElementById('Anterior');
 const Siguiente = document.getElementById('Siguiente');
